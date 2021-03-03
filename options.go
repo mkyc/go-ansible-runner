815196fa94dcad36ec01bf4397e3b5dacc6adf7c
Mateusz Kyc <mateusz.kyc@gmail.com>
2021-03-03 15:13:11 +0100

    add logs level to options.go

--- options.go
+++ options.go
@@ -1,5 +1,16 @@
 package gar
 
+type LogsLevel int
+
+const (
+	L1 LogsLevel = iota
+	L2
+	L3
+	L4
+	L5
+	L6
+)
+
 type Options struct {
 	AnsibleRunnerBinary string // Name of the binary that will be used.
 	AnsibleRunnerDir    string // The path to the directory where ansible-runner structure is defined.
@@ -7,7 +18,8 @@ type Options struct {
 	Playbook string // Name of playbook to run
 	Ident    string // ansible-runner identifier directory name
 
-	EnvVars map[string]string
+	EnvVars   map[string]string
+	LogsLevel LogsLevel
 
 	Logger Logger // Logger interface used
 }
@@ -18,6 +30,8 @@ func (o Options) clone() Options {
 		AnsibleRunnerDir:    o.AnsibleRunnerDir,
 		Playbook:            o.Playbook,
 		Ident:               o.Ident,
+		EnvVars:             o.EnvVars,
+		LogsLevel:           o.LogsLevel,
 		Logger:              o.Logger,
 	}
 }
@@ -39,6 +53,19 @@ func getCommonOptions(options Options, args ...string) (Options, []string) {
 	if options.Logger == nil {
 		resultOptions.Logger = DefaultLogger{}
 	}
-
+	switch options.LogsLevel {
+	case L1:
+		args = append(args, "-v")
+	case L2:
+		args = append(args, "-vv")
+	case L3:
+		args = append(args, "-vvv")
+	case L4:
+		args = append(args, "-vvvv")
+	case L5:
+		args = append(args, "-vvvvv")
+	case L6:
+		args = append(args, "-vvvvv", "--debug")
+	}
 	return resultOptions, args
 }
